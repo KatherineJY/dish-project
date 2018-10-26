9808ce482e162c208d5d4b1bee9f35f073c4d979
KatherineJY <kellymayliu@163.com>
2018-10-26 16:41:40 +0800

    initial commit

---
+++ src/component/Pcontent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Pcontent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Pcontent;
